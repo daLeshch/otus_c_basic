e23535fdb92f30baf214090e821074c88ee556ac
daLeshch <152603608+daLeshch@users.noreply.github.com>
2025-01-12 00:48:17 +0300

    tests ready

--- test/dl_list_tests.cpp
+++ test/dl_list_tests.cpp
@@ -1,17 +1,151 @@
 #include <gtest/gtest.h>
-#include "dl_list.cpp"
+#include "../dl_list.cpp"
 
-template <typename T>
-class DlListTest : public ::testing::Test{
-    protected:
-    ListDL<T>* container;
+#include <iostream>
 
-    void SetUp() override {
-        container = new ListDL()
-    };
+TEST(ListDL, empty) {
+    // Arrange
+    ListDL<int> container;
 
-    void TearDown() override {
-        delete container;
+    // Act (empty for this test)
+
+    // Assert
+    ASSERT_EQ(container.size(), 0);
+}
+
+
+TEST(ListDL, filled) {
+    // Arrange
+    ListDL<int> container;
+
+    // Act (empty for this test)
+    for (int i = 0; i <= 9; i++) {
+        container.push_back(i);
     }
 
+    // Assert
+    ASSERT_EQ(container.size(), 10);
+}
+
+TEST(ListDL, append) {
+    // Arrange
+    ListDL<int> container;
+    
+    // Act (empty for this test)
+
+    container.append(1);
+
+    container.append(15);
+
+    // Assert
+    ASSERT_EQ(container[0], 15);
+    ASSERT_EQ(container.size(), 2);
+}
+
+TEST(ListDL, push_back) {
+    // Arrange
+    ListDL<int> container;
+    
+    // Act (empty for this test)
+
+    container.push_back(1);
+
+    container.push_back(15);
+
+    // Assert
+    ASSERT_EQ(container[1], 15);
+    ASSERT_EQ(container.size(), 2);
+}
+
+TEST(ListDL, erase_first) {
+    // Arrange
+    ListDL<int> container;
+    
+    // Act (empty for this test)
+
+    for (int i = 0; i <= 9; i++) {
+        container.push_back(i);
+    }
+    container.erase(0);
+
+    // Assert
+    ASSERT_EQ(container[0], 1);
+    ASSERT_EQ(container.size(), 9);
+}
+
+TEST(ListDL, erase_last) {
+    // Arrange
+    ListDL<int> container;
+    
+    // Act (empty for this test)
+
+    for (int i = 0; i <= 9; i++) {
+        container.push_back(i);
+    }
+    container.erase(9);
+
+    // Assert
+    ASSERT_THROW(container[9], std::out_of_range);
+    ASSERT_EQ(container.size(), 9);
+}
+
+TEST(ListDL, erase_middle) {
+    // Arrange
+    ListDL<int> container;
+    
+    // Act (empty for this test)
+
+    for (int i = 0; i <= 9; i++) {
+        container.push_back(i);
+    }
+    container.erase(3);
+
+    // Assert
+    ASSERT_EQ(container[3], 4);
+    ASSERT_EQ(container.size(), 9);
+}
+
+TEST(ListDL, insert_middle) {
+    // Arrange
+    ListDL<int> container;
+    
+    // Act (empty for this test)
+
+    for (int i = 0; i <= 8; i++) {
+        container.push_back(i);
+    }
+    container.insert(20, 3);
+
+    // Assert
+    ASSERT_EQ(container[3], 20);
+    ASSERT_EQ(container.size(), 10);
+}
+
+TEST(ListDL, size) {
+    // Arrange
+    ListDL<int> container;
+    
+    // Act (empty for this test)
+
+    for (int i = 0; i <= 9; i++) {
+        container.push_back(i);
+    }
+
+    // Assert
+    ASSERT_EQ(container.size(), 10);
+}
+
+TEST(ListDL, insert_out_of_range) {
+    // Arrange
+    ListDL<int> container;
+    
+    // Act (empty for this test)
+
+    for (int i = 0; i <= 9; i++) {
+        container.push_back(i);
+    }
+
+    // Assert
+    ASSERT_THROW(container.insert(20, 11);, std::out_of_range);
+    ASSERT_EQ(container.size(), 10);
 }
--- test/seqContainer_tests.cpp
+++ test/seqContainer_tests.cpp
@@ -1,21 +1,151 @@
 #include <gtest/gtest.h>
-#include "seqContainer.cpp"
+#include "../seqContainer.cpp"
 
-template <typename T>
-class seqContainerTest : public ::testing::Test {
-    protected:
-    seqContainer<T>* container;
+#include <iostream>
 
-    void SetUp() override {
-        container = new seqContainer<T>(10);
+TEST(seqContainer, empty) {
+    // Arrange
+    seqContainer<int> container(10);
+
+    // Act (empty for this test)
+
+    // Assert
+    ASSERT_EQ(container.size(), 0);
+    ASSERT_EQ(container.capacity(), 10);
+}
+
+TEST(seqContainer, filled_no_reserve) {
+    // Arrange
+    seqContainer<int> container(10);
+    for (int i = 0; i <= 9; i++) {
+        container.push_back(i);
     }
 
-    void TearDown() override {
-        delete container;
-    }
-};
+    // Act (empty for this test)
 
-TEST_F(seqContainerTest<int>, IntInitialization) {
-    EXPECT_EQ(container->capacity(), 10);
-    EXPECT_EQ(container->size(), 0);
+    // Assert
+    ASSERT_EQ(container.size(), 10);
+    ASSERT_EQ(container.capacity(), 10);
+}
+
+TEST(seqContainer, filled_with_reserve) {
+    // Arrange
+    seqContainer<int> container(10, true);
+
+    // Act (empty for this test)
+    for (int i = 0; i <= 9; i++) {
+        container.push_back(i);
+    }
+
+    // Assert
+    ASSERT_EQ(container.size(), 10);
+    ASSERT_EQ(container.capacity(), 20);
+}
+
+TEST(seqContainer, append) {
+    // Arrange
+    seqContainer<int> container(10);
+    
+    // Act (empty for this test)
+
+    container.append(1);
+
+    container.append(15);
+
+    // Assert
+    ASSERT_EQ(container[0], 15);
+    ASSERT_EQ(container.size(), 2);
+}
+
+TEST(seqContainer, push_back) {
+    // Arrange
+    seqContainer<int> container(10);
+    
+    // Act (empty for this test)
+
+    container.push_back(1);
+
+    container.push_back(15);
+
+    // Assert
+    ASSERT_EQ(container[1], 15);
+    ASSERT_EQ(container.size(), 2);
+}
+
+TEST(seqContainer, erase_first) {
+    // Arrange
+    seqContainer<int> container(10);
+    
+    // Act (empty for this test)
+
+    for (int i = 0; i <= 9; i++) {
+        container.push_back(i);
+    }
+    container.erase(0);
+
+    // Assert
+    ASSERT_EQ(container[0], 1);
+    ASSERT_EQ(container.size(), 9);
+}
+
+TEST(seqContainer, erase_last) {
+    // Arrange
+    seqContainer<int> container(10);
+    
+    // Act (empty for this test)
+
+    for (int i = 0; i <= 9; i++) {
+        container.push_back(i);
+    }
+    container.erase(9);
+
+    // Assert
+    ASSERT_THROW(container[9], std::out_of_range);
+    ASSERT_EQ(container.size(), 9);
+}
+
+TEST(seqContainer, erase_middle) {
+    // Arrange
+    seqContainer<int> container(10);
+    
+    // Act (empty for this test)
+
+    for (int i = 0; i <= 9; i++) {
+        container.push_back(i);
+    }
+    container.erase(3);
+
+    // Assert
+    ASSERT_EQ(container[3], 4);
+    ASSERT_EQ(container.size(), 9);
+}
+
+TEST(seqContainer, insert_middle) {
+    // Arrange
+    seqContainer<int> container(10);
+    
+    // Act (empty for this test)
+
+    for (int i = 0; i <= 8; i++) {
+        container.push_back(i);
+    }
+    container.insert(20, 3);
+
+    // Assert
+    ASSERT_EQ(container[3], 20);
+    ASSERT_EQ(container.size(), 10);
+}
+
+TEST(seqContainer, size) {
+    // Arrange
+    seqContainer<int> container(10);
+    
+    // Act (empty for this test)
+
+    for (int i = 0; i <= 9; i++) {
+        container.push_back(i);
+    }
+
+    // Assert
+    ASSERT_EQ(container.size(), 10);
 }
